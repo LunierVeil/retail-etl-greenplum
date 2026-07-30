01fd843e2fd862f9efc31706dc89eb7656e4cde1
LunierVeil <egorka.bespalov.03@mail.ru>
2026-07-30 17:59:57 +0300

    Update 1_external_tables.sql

--- 1_external_tables.sql
+++ 1_external_tables.sql
@@ -1,23 +1,24 @@
 --gpfdist
-create external table std16_12fp.promo_types_ext(
+create external table retail_dwh.promo_types_ext(
 	promotion_type text,
-	text text
+	type_name text
 )
-location('gpfdist://172.16.128.50:8080/promo_types.csv')
+location('gpfdist://172.16.128.134:8080/promo_types.csv')
 on all 
 format 'csv' (delimiter ';' header )
 encoding 'utf8';
 
-create external table std16_12fp.store_ext(
+create external table retail_dwh.store_ext(
 	shop_id text,
 	shop_name text
 )
-location('gpfdist://172.16.128.50:8080/store*.csv')
+location('gpfdist://172.16.128.134:8080/store*.csv')
 on all 
 format 'csv' (delimiter ';' header )
 encoding 'utf8';
 
-create external table std16_12fp.coupons_ext(
+DROP EXTERNAL TABLE retail_dwh.coupons_ext;
+create external table retail_dwh.coupons_ext(
 	shop_id text,
 	calday date,
 	coupon_number varchar(7),
@@ -25,26 +26,26 @@ create external table std16_12fp.coupons_ext(
 	product_id int8,
 	receipt_id int8
 )
-location('gpfdist://172.16.128.50:8080/coupons*.csv')
+location('gpfdist://172.16.128.134:8080/coupons*.csv')
 on all 
 format 'csv' (delimiter ';' header )
 encoding 'utf8';
 
-create external table std16_12fp.promos_ext(
+create external table retail_dwh.promos_ext(
 	promos_id text,
 	names text,
 	type_promos int4,
 	product int8,
 	discount_amount int4
 )
-location('gpfdist://172.16.128.50:8080/promos*.csv')
+location('gpfdist://172.16.128.134:8080/promos*.csv')
 on all 
 format 'csv' (delimiter ';' header )
 encoding 'utf8';
 
 
 --pxf
-CREATE EXTERNAL table std16_12fp.traffic_ext(
+CREATE EXTERNAL table retail_dwh.traffic_ext(
 	plant bpchar(4),
 	"date" bpchar(10),
 	"time" bpchar(6),
@@ -56,7 +57,7 @@ location ('pxf://gp.traffic?PROFILE=JDBC&JDBC_DRIVER=org.postgresql.Driver&DB_UR
 FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import')
 encoding 'UTF8';
 
-CREATE EXTERNAL table std16_12fp.bills_head_ext(
+CREATE EXTERNAL table retail_dwh.bills_head_ext(
 	billnum int8,
 	plant bpchar(4),
 	calday date
@@ -66,7 +67,7 @@ location ('pxf://gp.bills_head?PROFILE=JDBC&JDBC_DRIVER=org.postgresql.Driver&DB
 FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import')
 encoding 'UTF8';
 
-CREATE EXTERNAL table std16_12fp.bills_item_ext(
+CREATE EXTERNAL table retail_dwh.bills_item_ext(
 	billnum int8,
 	billitem int8,
 	material int8,
@@ -80,7 +81,3 @@ location ('pxf://gp.bills_item?PROFILE=JDBC&JDBC_DRIVER=org.postgresql.Driver&DB
 ) on all
 FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import')
 encoding 'UTF8';
-
-
-
-
