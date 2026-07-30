b480519438891273bdb579cd80d3f942539bc1bf
LunierVeil <egorka.bespalov.03@mail.ru>
2026-07-30 18:00:12 +0300

    Update 2_target_tables.sql

--- 2_target_tables.sql
+++ 2_target_tables.sql
@@ -1,29 +1,29 @@
 -- начало
-CREATE SCHEMA std16_12fp;
+CREATE SCHEMA retail_dwh;
 -- проверка
-SELECT * FROM std16_12fp.promo_types_ext;
-SELECT * FROM std16_12fp.store_ext;
-SELECT * FROM std16_12fp.coupons_ext;
-SELECT * FROM std16_12fp.promos_ext;
-SELECT * FROM std16_12fp.traffic_ext LIMIT 3;
-SELECT * FROM std16_12fp.bills_head_ext LIMIT 3;
-SELECT * FROM std16_12fp.bills_item_ext LIMIT 3;
+SELECT * FROM retail_dwh.promo_types_ext;
+SELECT * FROM retail_dwh.store_ext;
+SELECT * FROM retail_dwh.coupons_ext;
+SELECT * FROM retail_dwh.promos_ext;
+SELECT * FROM retail_dwh.traffic_ext LIMIT 3;
+SELECT * FROM retail_dwh.bills_head_ext LIMIT 3;
+SELECT * FROM retail_dwh.bills_item_ext LIMIT 3;
 -- справочники
-create table std16_12fp.promo_types(
+create table retail_dwh.promo_types(
 	promotion_type text not null,
 	type_name text not null
 )
 distributed replicated;
 
-create table std16_12fp.stores(
+create table retail_dwh.stores(
 	shop_id text not null,
 	shop_name text not null
 )
 distributed replicated;
 
-create table std16_12fp.promos(
+create table retail_dwh.promos(
 	promos_id text not null,
-	names text,
+	names text NOT NULL,
 	type_promos int4 not null,
 	product int8 not null,
 	discount_amount int4 not null
@@ -31,34 +31,35 @@ create table std16_12fp.promos(
 distributed replicated;
 --факты
 --AO
-create table std16_12fp.coupons(
-	shop_id text not null,
-	coupon_date date not null,
-	coupon_number varchar(7),
-	promo_id text,
-	product_id int8 not null,
-	receipt_id int8 not null
+create table retail_dwh.coupons(
+    shop_id text NOT NULL,
+    calday date NOT NULL,
+    coupon_number varchar(7),
+    promo_id text NOT NULL,
+    product_id int8 NOT NULL,
+    receipt_id int8 NOT NULL
 )
 with(
-appendonly=true,
-orientation=column,
-compresstype=zstd,
-compresslevel=1
+    appendonly=true,
+    orientation=column,
+    compresstype=zstd,
+    compresslevel=1
 )
-distributed by(receipt_id)
-partition  by range (coupon_date)
+distributed by(shop_id, calday)
+partition by range (calday)
 (
    start ('2021-01-01') inclusive
-    end ('2025-01-01') exclusive
-    every (interval '1 month')
+   end ('2025-01-01') exclusive
+   every (interval '1 month'),
+   DEFAULT PARTITION other
 );
 
-create table std16_12fp.traffic(
-	plant bpchar(4) NULL,
-	calday date NULL,
-	caltime bpchar(6) NULL,
-	frame_id bpchar(10) NULL,
-	quantity int4 NULL
+create table retail_dwh.traffic(
+	plant bpchar(4) NOT NULL,
+	calday date NOT NULL,
+	caltime bpchar(6) NOT  NULL,
+	frame_id bpchar(10) NOT  NULL,
+	quantity int4 NOT NULL
 )
 with(
 appendonly=true,
@@ -66,19 +67,20 @@ orientation=column,
 compresstype=zstd,
 compresslevel=1
 )
-distributed by(plant,frame_id)
+distributed by(plant,calday)
 partition  by range (calday)
 (
    start ('2021-01-01') inclusive
     end ('2025-01-01') exclusive
-    every (interval '1 month')
+    every (interval '1 month'),
+    DEFAULT PARTITION other
 );
 
 
-create table std16_12fp.bills_head(
-	billnum int8 NULL,
-	plant bpchar(4) NULL,
-	calday date NULL
+create table retail_dwh.bills_head(
+	billnum int8 NOT NULL,
+	plant bpchar(4) NOT NULL,
+	calday date NOT NULL
 )
 with(
 appendonly=true,
@@ -86,22 +88,23 @@ orientation=column,
 compresstype=zstd,
 compresslevel=1
 )
-distributed by(billnum)
+distributed by(plant,calday)
 partition  by range (calday)
 (
    start ('2021-01-01') inclusive
     end ('2025-01-01') exclusive
-    every (interval '1 month')
+    every (interval '1 month'),
+    DEFAULT PARTITION other
 );
-create table std16_12fp.bills_item(
-	billnum int8 NULL,
-	billitem int8 NULL,
-	material int8 NULL,
-	qty int8 NULL,
-	netval numeric(17, 2) NULL,
-	tax numeric(17, 2) NULL,
-	rpa_sat numeric(17, 2) NULL,
-	calday date NULL
+create table retail_dwh.bills_item(
+	billnum int8 NOT NULL,
+	billitem int8 NOT NULL,
+	material int8 NOT NULL,
+	qty int8 NOT NULL,
+	netval numeric(17, 2) NOT NULL,
+	tax numeric(17, 2) NOT NULL,
+	rpa_sat numeric(17, 2) NOT NULL,
+	calday date NOT NULL
 )
 with(
 appendonly=true,
@@ -109,12 +112,13 @@ orientation=column,
 compresstype=zstd,
 compresslevel=1
 )
-distributed by(billnum)
+distributed by(billnum,billitem)
 partition  by range (calday)
 (
    start ('2021-01-01') inclusive
     end ('2025-01-01') exclusive
-    every (interval '1 month')
+    every (interval '1 month'),
+    DEFAULT PARTITION other
 );
 
 
